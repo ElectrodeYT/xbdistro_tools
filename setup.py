e34acaf23bf0dceacd510d50f2e4c4a7f479cf97
Alexander <electrodeyt@gmail.com>
2023-08-23 01:49:56 +0200

    Bump to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='XBDistroTools',
-    version='0.1.0',
+    version='0.2.1',
     packages=['XBStrapDistro', 'XBStrapSQLite'],
     url='https://electrode.codes',
     author='electrodeyt',
